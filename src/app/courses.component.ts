04f42b0b832feb57ea82cc009c666bc1bcef9bfa
Remi Cauchon <rcauchon@ca.ibm.com>
2019-04-30 17:32:19 -0400

    build on the cloud latest changes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,23 +5,40 @@ import { CoursesService } from './course/courses.service';
     selector: 'courses',
     template: `
         <h2>{{ getTitle() }}</h2>
+        <h2 [innerHTML] = 'title'></h2>
         <ul>
             <li *ngFor="let course of courses">
                 {{course}}
             </li>
         </ul>
+        <button (click) = 'toggleFlag()'>Adding click count</button>
+        <h2>count: {{toggle}}</h2>
+        <input [(ngModel)]="filter" (keyup.enter)="onKeyUp()"/> <p>{{input2}}</p>
     `
 })
 
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    toggle: number = 0;
+    filter;
+    input2 = 'blank';
     getTitle() {
         return this.title;
     }
 
+    toggleFlag() {
+        return this.toggle++;
+    }
+
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
+        this.filter = 'Alma';
+    }
+
+    onKeyUp() {
+        this.input2 = this.filter;
+        console.log(this.filter);
     }
     // Logic for calling an HTTP service
     // Only presentation logic
